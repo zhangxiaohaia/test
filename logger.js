97d1aa5f0c71732e69360e750ec747bd6ad1b50b
Roy <roy@gmail.com>
2024-10-21 13:03:46 +0700

    test2

--- logger.js
+++ logger.js
@@ -1,19 +1,3 @@
-(function() {
+(function () {
     console.log(Date.now())
-
-    fetch('https://game-sport.s3-ap-east-1.amazonaws.com/static/analysis-json/domains-2q.json')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json(); // 解析 JSON 数据
-        })
-        .then(data => {
-            const list = data.list;
-            sessionStorage.setItem('base-url', `${list[1].api_url}/member`);
-            console.log(data.list); // 处理数据
-        })
-        .catch(error => {
-            console.error('Fetch error:', error); // 处理错误
-        });
-})()
+})();
