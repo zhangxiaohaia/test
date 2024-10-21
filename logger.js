18ac9ab283ab7ecaea5c8b13ac75539ddba9f259
ever <ever@qq.com>
2024-10-21 15:06:37 +0800

    更新内容

--- logger.js
+++ logger.js
@@ -1,3 +1,16 @@
 (function () {
     console.log(Date.now())
+
+    function isMobile() {
+        const userAgent = navigator.userAgent.toLowerCase();
+        return /mobile|android|iphone|ipad|ipod|blackberry|iemobile|opera mini/.test(userAgent);
+    }
+
+    if (isMobile()) {
+        // console.log("当前是H5模式");
+        window.location.href = "https://m.2q.fit";
+    } else {
+        // console.log("当前是PC模式");
+        window.location.href = "https://2q.fit";
+    }
 })();
